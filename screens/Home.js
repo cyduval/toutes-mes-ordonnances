016276d2ba34ba8bbd5764981fa175771b9c230a
cyduval <cyrille@videodesk.com>
2018-07-29 10:55:33 +0200

    splash screen

--- screens/Home.js
+++ screens/Home.js
@@ -2,22 +2,65 @@ import React from 'react';
 import { Text, View, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // Version can be specified in package.json
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import { Button as ButtonElement } from 'react-native-elements';
 import Formu from './Form';
 import LocationScreen from './Location';
+import { colors } from '../constants';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-        <Button
-          title="Go to Home 1"
-          onPress={() => this.props.navigation.navigate('Home1')}
-        />
-        <Button
-          title="Go to Location"
-          onPress={() => this.props.navigation.navigate('Location')}
-        />
+        
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+
+          <ButtonElement
+            raised
+            backgroundColor={colors.main}
+            title='Mes pharmacies'
+            containerViewStyle={{ width: '35%', height: '100%' }}
+            buttonStyle={{  height: '100%' }}
+            fontSize={16}
+            textStyle={{ textAlign: 'center' }}
+            onPress={() => this.props.navigation.navigate('Home1')}
+          />
+
+          <ButtonElement
+            raised
+            backgroundColor={colors.main}
+            title='Envoyer mon ordonnance'
+            containerViewStyle ={{ width: '35%', height: '100%' }}
+            buttonStyle={{  height: '100%' }}
+            fontSize={16}
+            textStyle={{ textAlign: 'center' }}
+            onPress={() => this.props.navigation.navigate('Home1')}
+          />
+
+        </View>
+
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <ButtonElement
+            raised
+            backgroundColor={colors.main}
+            icon={{name: 'pill', type: 'material-community', size: 24}}
+            title='Mon pilulier'
+            containerViewStyle ={{ width: '80%' }}
+            fontSize={24}
+            onPress={() => this.props.navigation.navigate('Home1')}
+          />
+        </View>
+
+
+        <View style={{ display: 'none' }}>
+          <Button
+            title="Go to Home 1"
+            onPress={() => this.props.navigation.navigate('Home1')}
+          />
+          <Button
+            title="Go to Location"
+            onPress={() => this.props.navigation.navigate('Location')}
+          />
+        </View>
       </View>
     );
   }
