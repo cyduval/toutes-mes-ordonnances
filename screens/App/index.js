3d6c04bc086ad93c1510c70550499f07a116d263
cyduval <cyrille@videodesk.com>
2018-07-30 21:05:08 +0200

    onboarding

--- screens/App/index.js
+++ screens/App/index.js
@@ -1,112 +1,91 @@
 import React from 'react';
 import firebase from 'firebase';
 import Sentry from 'sentry-expo';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { NetInfo, Image, AsyncStorage } from 'react-native';
-import Onboarding from 'react-native-onboarding-swiper';
+import {
+  compose
+} from 'redux';
+import {
+  connect
+} from 'react-redux';
+import {
+  NetInfo,
+  Image,
+  AsyncStorage
+} from 'react-native';
+import Onboarding from '../OnBoarding';
 import Home from '../Home';
 import Auth from '../Auth';
 import NoNetwork from '../NoNetwork';
-import { loginSuccess } from '../Auth/Login/actions';
+import {
+  loginSuccess
+} from '../Auth/Login/actions';
 import Loading from '../../components/Loading';
 
 class AppScreen extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.onFinish = this.onFinish.bind(this);
-  
-      this.state = {
-        finish: false,
-        onboarding: false,
-      };
-      // Remove this once Sentry is correctly setup.
-      Sentry.enableInExpoDevelopment = true;
-      Sentry.config('https://1453d539784e4f92982c22a166afa5a7@sentry.io/1246851').install();
-    }
-  
-    componentWillMount() {
-      NetInfo.getConnectionInfo().then((connectionInfo) => {
-        console.log('Initial, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType);
-      });
-      this._getData();
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-      const user = firebase.auth().currentUser;
-      // alert(user);
-    }
-  
-    _storeData = async () => {
-      try {
-        await AsyncStorage.setItem('onboarding', true);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  
-    _getData = async () => {
-      const value = await AsyncStorage.getItem('onboarding');
-      this.setState({ onboarding: value });
-    }
-  
-    onFinish() {
-      this._storeData();
-      this.setState({ finish: true, onboarding: true });
-    }
-  
-    render() {
-      const { auth, global } = this.props;
-
-      console.log(112233);
-      console.log(auth);
-
-      if (global.isNetwork === 'none' || global.isNetwork === 'unknown' || global.isNetwork === 'undefined') {
-        return (<NoNetwork />);
-      }
-
-      if(!auth.user) {
-        return (<Auth />);
-      }
-  
-      if(!auth.firebaseLoaded) {
-        return (<Loading />);
-      }
-      
-      return (<Home />);
-  
-      // if (this.state.finish || this.state.onboarding) {
-        return (<Home />);
-      // }
-  
-      return (
-      <Onboarding
-        onDone={this.onFinish}
-        onSkip={this.onFinish}
-        pages={[
-          {
-            backgroundColor: '#fff',
-            image: <Image source={require('../../assets/1.png')} />,
-            title: 'Onboarding 1',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-          {
-            backgroundColor: '#fff',
-            image: <Image source={require('../../assets/2.png')} />,
-            title: 'Onboarding 2',
-            subtitle: 'eeeeeee',
-          },
-        ]}
-      />
-      );
-  
-    }
+    this.state = {
+      onboarding: 'no',
+    };
+    // Remove this once Sentry is correctly setup.
+    Sentry.enableInExpoDevelopment = true;
+    Sentry.config('https://1453d539784e4f92982c22a166afa5a7@sentry.io/1246851').install();
   }
-  
+
+  componentWillMount() {
+    NetInfo.getConnectionInfo().then((connectionInfo) => {
+      console.log('Initial, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType);
+    });
+    this._getData();
+  }
+
+  _getData = async () => {
+    const value = await AsyncStorage.getItem('onboarding');
+    this.setState({
+      onboarding: value
+    });
+  }
+
+  onboardingFinish = () => {
+    console.log(9999);
+    this.setState({
+      onboarding: 'yes'
+    });  
+  }
+
+  render() {
+    const {
+      auth,
+      global
+    } = this.props;
+
+    if (global.isNetwork === 'none' || global.isNetwork === 'unknown' || global.isNetwork === 'undefined') {
+      return ( < NoNetwork / > );
+    }
+
+    if (!auth.user) {
+      return ( < Auth / > );
+    }
+
+    if (!auth.firebaseLoaded) {
+      return ( < Loading / > );
+    }
+
+    console.log('onboarding => ', this.state.onboarding);
+    if (this.state.onboarding !== 'yes') {
+      return ( < Onboarding onboardingFinish={this.onboardingFinish} / > );
+    }
+
+    return ( < Home / > );
+
+  }
+}
 
 const mapStateToProps = state => {
-  return { ...state };
+  return { ...state
+  };
 };
 
 function mapDispatchToProps(dispatch) {
@@ -116,10 +95,10 @@ function mapDispatchToProps(dispatch) {
 }
 
 const withConnect = connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  );
+  mapStateToProps,
+  mapDispatchToProps,
+);
 
 export default compose(
-    withConnect,
-  )(AppScreen);
+  withConnect,
+)(AppScreen);
